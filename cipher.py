205b0381218e55bccbe99d19e907a4060eee58b2
cheralynli <cheralynlianto@gmail.com>
2025-10-09 01:31:16 +0800

    Decrypt done

--- cipher.py
+++ cipher.py
@@ -1,8 +1,10 @@
 def encrypt(plaintext,keyshift):
     cipher=""
     for i in range(len(keyshift)):
-        x=(ord(plaintext[i])+ord(keyshift[i]))%26 #mod 26 to cycle back to A-Z
+        # get ASCII value, add keyshift ASCII value, mod 26 to cycle back to A-Z
+        x=(ord(plaintext[i])+ord(keyshift[i]))%26
         x+=ord('A')
+        # append character to cipher
         cipher+=chr(x)
     return cipher
 
@@ -11,12 +13,14 @@ def decrypt(ciphertext,key):
     keyshift=key
     for i in range(len(ciphertext)):
         if i<len(key):
+            #get ASCII value of ciphertext and key at i for first len(key) characters
             x=(ord(ciphertext[i])-ord(key[i])+26)%26
             x+=ord('A') 
             plaintext+=chr(x)
             keyshift+=chr(x)
         else:
-            y=(ord(ciphertext[i])-ord(keyshift[i])+26)%26
+
+            y=(ord(ciphertext[i])-ord(plaintext[i-len(key)])+26)
             y+=ord('A')
             plaintext+=chr(y)
             keyshift+=chr(y)
